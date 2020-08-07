325a92a21de482f427377a5e7b2d8b352f06b5d0
林一鸣 <linyiming@chinatelecom.cn>
2020-08-07 16:33:00 +0800

    echo

--- src/com/ecloud/pipeline/Pipelines.groovy
+++ src/com/ecloud/pipeline/Pipelines.groovy
@@ -7,12 +7,13 @@ class Pipelines {
     Context context = new Context(jenkins, pipelineParameters)
     String jobName=jenkins.env.JOB_NAME
     String jobType=getJobType(jobName)
+    jenkins.echo "jobType====="+jobType
     switch (jobType){
       case jobType == "verify":
-            println("verifyci")
+            jenkins.echo "verifyci"
             new CCIPipelineBuilder().build(context)
       default:
-            println("defaultci")
+            jenkins.echo "defaultci"
             new DefaultPipelineBuilder().build(context)
     }
   }
@@ -21,7 +22,6 @@ class Pipelines {
     String [] jobSplit=jobName.split("_");
     int jobSplitLength=jobSplit.length;
     String jobType=jobSplit[jobSplitLength-1]
-    println("jobType==" + jobType)
     return jobType
   }
 }
